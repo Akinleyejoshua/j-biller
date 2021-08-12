85b32d1c61d4bdaa526dcdcf40ff28d1a4d31a59
Joshua <akinleyejoshua.dev@gmail.com>
2021-08-12 10:10:11 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ $.ajax({
   headers: {
     'Access-Control-Allow-Origin' : '*',
     "Authorization": "Bearer FLWSECK-f9907f31558f7bfc64e5eeb46e7a53b2-X",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization, Verification"
   },
   data: JSON.stringify({
     card_number: "4556052704172643",
